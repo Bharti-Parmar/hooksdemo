d513e08b6deec15524cf484fad24353bf67b4c3e
bhartip <bharti.parmar@agnity.com>
2022-11-02 23:10:56 +0530

    code file changes commit

--- src/components/OnOffLineConcept.jsx
+++ src/components/OnOffLineConcept.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 
 function OnOffLineConcept() {
     // const [status, setStatus] = useState('Loading...');
-    const [seconds, setSeconds] = useState(0);
 
     // useEffect(() => {
     //     const timer = setInterval(() => {
@@ -12,17 +11,9 @@ function OnOffLineConcept() {
     //     return clearInterval(timer);
     // },[])
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setSeconds(seconds => seconds + 1);
-        },1000)
-        return () => clearInterval(timer);
-    },[])
-
     return (
         <div>
             {/* <h3>{status}</h3> */}
-           <h3>4. Time in Seconds: {seconds}</h3> 
         </div>
     )
 }
---
+++ src/components/SetTimeInterval.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+
+function OnOffLineConcept() {
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+        },1000)
+        return () => clearInterval(timer);
+    },[])
+
+    return (
+        <div>
+           <h3>4. Time in Seconds: {seconds}</h3> 
+        </div>
+    )
+}
+
+export default OnOffLineConcept
